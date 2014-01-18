838fc7b32da13de00e981e0b0fefc0e6a67dd40e
Hack Reactor Students <nickescallon@gmail.com>
2014-01-17 19:18:14 -0800

    Successfully detecting collisions.

--- js/watchout.js
+++ js/watchout.js
@@ -13,26 +13,26 @@ var svg = d3.select('body').append('svg')
 
 
 var step = function(){
-var enemies = svg.selectAll('.enemy').data(d3.range(enemiesCount));
+  var enemies = svg.selectAll('.enemy').data(d3.range(enemiesCount));
 
-enemies.enter()
-  .append('circle')
-  .attr('class', 'enemy')
-  .attr('r', 10.5)
-  .attr('cx', function(){
-    return Math.random() * w;
-  })
-  .attr('cy', function(){
-    return Math.random() * h;
-  })
-  .attr('fill', 'black');
+  enemies.enter()
+    .append('circle')
+    .attr('class', 'enemy')
+    .attr('r', 10.5)
+    .attr('cx', function(){
+      return Math.random() * w;
+    })
+    .attr('cy', function(){
+      return Math.random() * h;
+    })
+    .attr('fill', 'black');
 
 
-enemies.transition().duration(enemySpeed).attr('cx', function(){
-    return Math.random() * w;
-  })
-  .attr('cy', function(){
-    return Math.random() * h;
+  enemies.transition().duration(enemySpeed).attr('cx', function(){
+      return Math.random() * w;
+    })
+    .attr('cy', function(){
+      return Math.random() * h;
   });
 };
 
@@ -59,14 +59,54 @@ player.on('mouseup', function(event){
   svg.on('mousemove', function(){});
 });
 
-// svg.on('mouseup', function(){
-//     var position = d3.mouse(this);
-//     player.attr('cx', position[0])
-//       .attr('cy', position[1]);
-// });
-
-
 setInterval(function(){
   //enemySpeed = enemySpeed-;
   step();
 }, 1000);
+
+//Collision Detection
+
+var checkCollisions = function(){
+    //console.log('checking');
+  var playerX = Number(d3.selectAll('.player').attr('cx'));
+  var playerY = Number(d3.selectAll('.player').attr('cy'));
+
+
+  d3.selectAll('.enemy').each(function(){
+    var enemyX = this.cx.animVal.value;
+    var enemyY = this.cy.animVal.value;
+    //debugger;
+    //console.log(Math.abs(enemyX - playerX));
+    if (Math.abs(enemyX - playerX) < 10 && Math.abs(enemyY-playerY) < 10){
+      console.log('COLLISION!');
+    }
+  });
+};
+
+setInterval(function(){
+  checkCollisions();
+}, 10);
+
+
+
+//playerX === enemyX && playerY === enemyY
+
+//Math.abs(enemyX - playerX) < 5 && Math.abs(enemyY-playerY) < 5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
